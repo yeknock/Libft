0e7950d9ad6b6e8fbf8539400bf1b2abde7e00a1
yeknock <153378352+yeknock@users.noreply.github.com>
2024-02-24 11:17:00 +0400

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,24 +1,29 @@
 CC = cc
 FLAGS = -Wall -Wextra -Werror
 SOURCE = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_atoi.c ft_bzero.c ft_calloc.c ft_isascii.c ft_isprint.c ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c ft_strchr.c  ft_strlen.c ft_strncmp.c ft_strnstr.c ft_strlcpy.c ft_strlcat.c ft_strrchr.c ft_tolower.c ft_toupper.c ft_strdup.c ft_itoa.c ft_strjoin.c ft_strmapi.c ft_putchar_fd.c ft_putstr_fd.c ft_substr.c ft_strtrim.c ft_split.c ft_striteri.c ft_putendl_fd.c ft_putnbr_fd.c
+BONUS_SOURCE = ft_lstnew.c
+BONUS_OBJECTS = $(BONUS_SOURCE:.c=.o)
 OBJECTS = $(SOURCE:.c=.o)
 LIBNAME = libft.a
 
 
 all: $(LIBNAME)
- 
+
+bonus: $(OBJECTS) $(BONUS_OBJECTS)
+	ar rcs $(LIBNAME) $(OBJECTS) $(BONUS_OBJECTS)
+
 $(LIBNAME): $(OBJECTS)
-	ar -rcs $(LIBNAME) $(OBJECTS)
+	ar rcs $(LIBNAME) $(OBJECTS)
 
 %.o: %.c
 	$(CC) $(FLAGS) -c $< -o $@
 
 clean:
-	rm -f $(OBJECTS)
+	rm -f $(OBJECTS) $(BONUS_OBJECTS)
 
 fclean: clean
-	rm -rf $(LIBNAME)
+	rm -f $(LIBNAME)
 
 re: fclean all
 
-.PHONY: all clean fclean re $(LIBNAME)
+.PHONY: all bonus clean fclean re $(LIBNAME)
---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymartiro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 14:58:41 by ymartiro          #+#    #+#             */
+/*   Updated: 2024/02/23 14:58:48 by ymartiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*ret;
+
+	ret = malloc(sizeof(t_list));
+	if (!ret)
+		return (0);
+	ret -> content = content;
+	ret -> next = NULL;
+	return (ret);
+}
